3777471bd51c345dd39a5ab8ec7c171fe4972d98
pawan <thakunipawan@gmail.com>
2025-03-17 12:20:56 +0530

    hooks

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+1--whar are single page application-->single page applications are used in web app 's it has single HTML page ,this html page changed as user interact with the website{example--linkediN}
+2--npm create vite@latest
+2--npm install 
+3--npm run dev
+==============================================================================================
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+4--import {browseRouter,routes,route} from react-router-dom
+5--use{Link to="/" for make spa ,to check go in network tab there will be no html rendering}==const navigate=useNavigate()==>navigate("/") we can do this.
+6--route path *              =>to get error
+7--Layouts=={outlet} ==> 
+const layout=()=>{
+    header
+
+    <outlet>{many routes or component will be here}
+    footer
+}
+=========================================================================================
+
+useRef==>
+const inputRef=useRef()
+<input ref={inputRef}></input>
--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,47 @@
 
 
 //this was the frist day to write some code and we just have created simple connect wallet with airdrop functionaility
-import { useState } from 'react'
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import '@solana/wallet-adapter-react-ui/styles.css';
-import './App.css'
-import { Airdrop } from './component/Airdrop.jsx';
 
+import { useState } from "react"
+
+//custom hook
+//today we learn coustom hook==>it is a powerful feature in react that let us encapsulate and reuse some stateful logic acroos our components
+
+
+
+function useCount(){
+  const [count,setCount]=useState(0)
+
+  function countnum(){
+    setCount(count=>count+1);
+  }
+  return {
+    count:count,
+    countnum:countnum
+  }
+
+
+}
 function App() {
   
+  
+ 
+  return <div>
+   frgrg
+  <CountNumber/>
+  
+  </div>
+}
 
-  return (
-    
-    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/gnPqgj4E0NnQ6gD1S_DITJx_Wdwp7L8O"}>
-    <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-            <WalletMultiButton />
-            <WalletDisconnectButton />
-            { /* Your app's components go here, nested within the context providers. */ }
-            <Airdrop></Airdrop>
-        </WalletModalProvider>
-    </WalletProvider>
-</ConnectionProvider>
-    
-  )
+function CountNumber(){
+
+  const{count,countnum}=useCount();
+
+  return <div> <button style={{color:"white"}}  onClick={countnum}>count :{count} </button>
+  </div>
 }
 
 export default App
+
+
+
